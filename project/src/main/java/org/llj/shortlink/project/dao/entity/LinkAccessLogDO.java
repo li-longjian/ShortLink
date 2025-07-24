ac9dd1ef5a9903189a2dacd04b4749b3440d0258
li-longjian <948867468@qq.com>
2025-07-24 16:11:15 +0800

    feature: 开发短连接监控: 日志

---
+++ project/src/main/java/org/llj/shortlink/project/dao/entity/LinkAccessLogDO.java
@@ -0,0 +1,23 @@
+package org.llj.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.llj.shortlink.project.common.dataBase.BaseDO;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_link_access_logs")
+public class LinkAccessLogDO extends BaseDO {
+    private Long id;
+    private String fullShortUrl;
+    private String gid;
+    private String browser;
+    private String user;
+    private String ip;
+    private String os;
+}
