c5fb9a43aedf15ccc0be48fa296d352b8a27507b
li-longjian <948867468@qq.com>
2025-07-29 16:33:39 +0800

    feature: 对项目进行微服务重构：nacos

--- admin/src/main/java/org/llj/shortlink/admin/AdminApplication.java
+++ admin/src/main/java/org/llj/shortlink/admin/AdminApplication.java
@@ -2,9 +2,12 @@ package org.llj.shortlink.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
+@EnableFeignClients("org.llj.shortlink.admin.remote")
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
